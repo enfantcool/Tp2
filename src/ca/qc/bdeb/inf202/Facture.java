9f4d56a877b2a84925de8c1b2df9243571b42075
Mart Greg <enfantcool@hotmail.com>
2012-08-28 01:02:37 -0400

    First

---
+++ src/ca/qc/bdeb/inf202/Facture.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.qc.bdeb.inf202;
+
+/**
+ *Création dùne facture
+ * @author Martin Grégoire
+ */
+public class Facture {
+   public static void getTps(){
+
+}
+   public static void getTvq(){
+       
+   }
+   public static void getPrix(){
+       
+   }
+}

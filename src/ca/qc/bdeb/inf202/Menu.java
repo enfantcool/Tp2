026222dffd665d6f12788c5d9894a1f7f9b0db17
Usager <Usager@LI-H017-0T11>
2012-09-11 15:11:10 -0400

    Menu

--- src/ca/qc/bdeb/inf202/Menu.java
+++ src/ca/qc/bdeb/inf202/Menu.java
@@ -4,8 +4,7 @@
  */
 package ca.qc.bdeb.inf202;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Opérations avec le menu
@@ -18,44 +17,117 @@ public class Menu {
      * tableau 2d qui contient le menu, commencant par le nom de l'item et par
      * les ingrédients et le prix dans laa deuxieme dimension
      */
-    protected HashMap<String,String> menu;
+    protected HashMap<String, HashMap<String, String>> menu;
+    /**
+     * Listes des items pouvant être commandés
+     */
+    private List<String> items;
     /**
      * table de hashage contenant les prix des items
      */
-    protected HashMap<String,Double> prix;
+    protected HashMap<String, String> prix;
 
     /**
-     * Met le menu sous forme de table de hashage dont la clé est le bnom de litem et la valeur étant la liste des ingrédients + le prix
+     * Met le menu sous forme de table de hashage dont la clé est le bnom de
+     * litem et la valeur étant la liste des ingrédients + le prix
      *
      * @param cheminfichier chemin du fichier donnant le menu
      */
     public Menu(String cheminFichier) {
         List<String> temp;
+        items = new ArrayList<String>();
+        prix = new HashMap<String, String>();
+        menu = new HashMap<String, HashMap<String, String>>();
         temp = getListe(cheminFichier);
-        triListe();
+        triListe(temp);
+        for (String out : items) {
+            System.out.println(out);
+        }
+
     }
 
     /**
      * Transforme la liste pour la mettre dans la table de hashage
+     *
      * @param cheminFichier chemin du fichier qui contient le menu
      * @return le fichier
      */
     protected List getListe(String cheminFichier) {
-        return null;
+
+        return Fichiers.readFile(cheminFichier);
     }
 
     /**
-     * remet la liste venant du fichier en tableau 2d de type String
+     * sépare la liste dans une liste contenant les noms des items et dans une
+     * HashMap contenant le prix des items.
      */
-    protected void triListe() {
+    protected void triListe(List<String> parcours) {
+        String dernierItem = "";
+        HashMap<String, String> table = new HashMap<String, String>();
+        for (int i = 0; i < parcours.size(); i++) {
+            char temp;
+            temp = parcours.get(i).charAt(0);
+            if (Character.isUpperCase(temp)) {
+                dernierItem = parcours.get(i);
+                items.add(dernierItem);
+
+            } else if (Character.isDigit(temp)) {
+
+
+                prix.put(dernierItem, parcours.get(i));
+                System.out.println(dernierItem);
+                for (String out:table.keySet()) {
+                    System.out.println(out+";"+table.get(out)+"----------");
+                }
+                menu.put(dernierItem, table);
+                System.out.println(menu.get(dernierItem).get("ketchup")+"88888888888");
+                table = new HashMap<String, String>();
+            } else {
+                List<String> list = new ArrayList<String>();
+                StringTokenizer decoupe = new StringTokenizer(parcours.get(i), ";");
+                while (decoupe.hasMoreTokens()) {
+                    list.add(decoupe.nextToken());
+                }
+                String premier = list.get(0);
+                list.remove(0);
+                table.put(premier, list.get(0));
+                
+            }
+
+        }
     }
+
     /**
      * cherche le prix dun item
-     * @param nomitem nom de litem
+     *
+     * @param nomitem nom de l'item
      * @param quantite quantite
      * @return prix
      */
-    public double getPrix(String nomitem,int quantite){
-        return 0;
+    public double getPrix(String nomItem, int quantite) {
+        String temp = prix.get(nomItem);
+        temp = temp.replace(',', '.');
+        temp = temp.replace('$', ' ');
+        temp = temp.trim();
+        return (Double.parseDouble(temp) * quantite);
+    }
+
+    public String getQuantite(String nomIngredient, String nomItem) {
+        System.out.println("------------000");
+        HashMap<String,String> table=new HashMap<String,String>();
+        table=menu.get(nomItem);
+        for (String out:table.keySet()) {
+            System.out.println(out);
+        }
+        return (menu.get(nomItem).get(nomIngredient));
+    }
+
+    public void test() {
+ 
+        for (String out : items) {
+            System.out.println(out);
+            System.out.println(getPrix(out, 1));
+        }
+        System.out.println(getQuantite("frites","Frite"));
     }
 }
--- src/ca/qc/bdeb/inf202/tp2.java
+++ src/ca/qc/bdeb/inf202/tp2.java
@@ -1,61 +1,70 @@
-
 package ca.qc.bdeb.inf202;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /*
- * D'abord, à l'ouverture, le programme charge les fichiers, effectue le renouvellement de son inventaire, et est prêt à être utilisé.
- * Ensuite, il demande à l'utilisateur ce qu'il veut faire. Si l'utilisateur veut faire une commande, alors le programme lui demande quels items, et en quelle quantite, 
- * produit ensuite une liste pour confirme, et envoie la commande a la facture et reduit l'inventaire des ingredients
- * . À chaque fois qu'un item est rajouté à la commande, le programme se crée un tableau "bidon" dans lequel les quantités diminuent,
- * évitant ainsi de pouvoir commander des items alors qu'ils ne reste pas assez d'ingrédiants pour les produire.
- * Comme autre choix, l'uttilisateur peut signaler une perte, alors elle est automatiquement enlevée de l'inventaire
- * et regarde si elle doit commander.
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ * D'abord, à l'ouverture, le programme charge les fichiers, effectue le
+ * renouvellement de son inventaire, et est prêt à être utilisé. Ensuite, il
+ * demande à l'utilisateur ce qu'il veut faire. Si l'utilisateur veut faire une
+ * commande, alors le programme lui demande quels items, et en quelle quantite,
+ * produit ensuite une liste pour confirme, et envoie la commande a la facture
+ * et reduit l'inventaire des ingredients . À chaque fois qu'un item est rajouté
+ * à la commande, le programme se crée un tableau "bidon" dans lequel les
+ * quantités diminuent, évitant ainsi de pouvoir commander des items alors
+ * qu'ils ne reste pas assez d'ingrédiants pour les produire. Comme autre choix,
+ * l'uttilisateur peut signaler une perte, alors elle est automatiquement
+ * enlevée de l'inventaire et regarde si elle doit commander.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
-
-
 /**
  * Travail #2
+ *
  * @author Martin Grégoire
  */
 public class tp2 {
 
-  //Code d'ouverture
+    //Code d'ouverture
     public static void main(String[] args) {
+
         String[] temp;
-        temp=Fichiers.toString("ingredients.txt");
-        
+        temp = Fichiers.toString("ingredients.txt");
+
         for (int i = 0; i < temp.length; i++) {
             System.out.println(temp[i]);
-            
+
         }
         System.out.println("------------------");
-        ingredients temp2=new ingredients("ingredients.txt");
-        temp2.setQuantite("frites",1000);
-        System.out.println(temp2.getQuantite("frites",0));
+        ingredients temp2 = new ingredients("ingredients.txt");
+        temp2.setQuantite("frites", 1000);
+        System.out.println(temp2.getQuantite("frites", 0));
         System.out.println("---------------");
         List<String> temp3;
-        temp3=temp2.getNoms();
+        temp3 = temp2.getNoms();
         for (int i = 0; i < temp3.size(); i++) {
             System.out.println(temp3.get(i));
-            
+
         }
- 
-    }
-    /**
-     * chargement des fichiers, attributtion des fiuchiers aux variables tels qu'ingrédients, menus...
-     */
-    public static void ouverture(){
+        System.out.println("-----------");
+        Menu blabla = new Menu("menu.txt");
+        System.out.println("-------------");
+        blabla.test();
+        System.out.println("---------");
         
+    }
+
+    /**
+     * chargement des fichiers, attributtion des fiuchiers aux variables tels
+     * qu'ingrédients, menus...
+     */
+    public static void ouverture() {
     }
 }
